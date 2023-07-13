ef66d8ce214d5f3cce01775a39d3753017107964
Michael Cherchi <mjcherchi@gmail.com>
2023-07-13 11:36:07 -0500

    iuhy

--- src/Lab6.ino
+++ src/Lab6.ino
@@ -3,36 +3,52 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
-uint16_t reading = analogRead(A4);
-double voltage = (reading * 3.3) / 4095.0;
-double temperatureC = (voltage - 0.5) * 100;
-double temperatureF = (temperatureC)*1.8 + 32;
+#define TEMP A4
 
 OledWingAdafruit display;
 
 void setup()
 {
-  pinMode(A4, INPUT);
+  pinMode(TEMP, INPUT);
+  display.setup();
 }
 
 void loop()
 {
   display.loop();
   {
-    display.setup();
+    uint16_t reading = analogRead(TEMP);
+    double voltage = (reading * 3.3) / 4095.0;
+    double temperatureC = (voltage - 0.5) * 100;
+    double temperatureF = (temperatureC)*1.8 + 32;
+
     display.clearDisplay();
 
-    display.display();
-    display.setTextSize(3);
+    display.setTextSize(2);
     display.setTextColor(WHITE);
     display.setCursor(0, 0);
     display.println(temperatureF);
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 16);
+    display.println(temperatureC);
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(65, 0);
+    display.println("F");
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(65, 16);
+    display.println("C");
+
     display.display();
   }
-  delay(100000000);
 }
 
-// OledWingAdafruit display;
+// hi
 
 // void setup() {
 // display.setup();
